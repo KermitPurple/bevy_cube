92aebee3b976de78a406e1d7f6c9c53919061895
Shane McDonough <shanemcdman@gmail.com>
2021-08-02 12:42:46 -0700

    Change window position

--- src/main.rs
+++ src/main.rs
@@ -35,7 +35,12 @@ fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
+    mut windows: ResMut<Windows>,
 ) {
+    // change window position
+    let window = windows.get_primary_mut().unwrap();
+    window.set_position([0, 0].into());
+
     // create the camera
     let mut camera = OrthographicCameraBundle::new_3d();
     camera.orthographic_projection.scale = 3.0;
@@ -59,7 +64,7 @@ fn setup(
 
 fn main() {
     App::build()
-        .insert_resource(Msaa { samples: 4 })
+        .insert_resource(Msaa { samples: 4 }) // don't actually know what this does but I should find out
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup.system())
         .add_system(Rotator::rotator.system())
